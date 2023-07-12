20a39c32cb4379d7dcdbda9a02836ed25e818aa5
viswakirankathi <viswauiux@gmail.com>
2023-07-12 21:47:49 +0530

    y

--- src/App.js
+++ src/App.js
@@ -2,24 +2,11 @@ import './App.css';
 import { useEffect, useState } from 'react';
 
 function App() {
-  var [count,setCount]=useState(0)
-  var t1 = setInterval(() => {
-    setCount(count+1)
-
-    
-  }, 1000);
-  
-  useEffect(()=>{
-    console.log(`After Mounting ${count}`);
-    return()=>{
-      clearInterval(t1)
-      console.log(`Before Mounting ${count}`);
-                }
-
-        })
+ 
+  const num =5;
   return (
     <div className="App">
-      <h1>{count}</h1>
+      <h1>{num}</h1>
     </div>
   );
 }
