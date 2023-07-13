0f164c0a4040308cf304e88839c20ca21091020b
viswakirankathi <viswauiux@gmail.com>
2023-07-13 07:36:26 +0530

    yes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import './App.css';
 import { useEffect, useState } from 'react';
 
 function App() {
-  for(let i =0;i<=10;i++){
-    if(i===5)continue;
-    console.log(i);
-  }
+var a= function (){}
+
+console.log(typeof a)
   return (
     <div className="App">
-       
+       HELLO
     </div>
   );
 }
