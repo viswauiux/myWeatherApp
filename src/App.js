063a9d610f83ba44de4cbdf6bb7ab15cbf9c3d64
viswakirankathi <viswauiux@gmail.com>
2023-07-20 07:06:17 +0530

    yes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { useEffect, useState } from 'react';
 
 function App() {
-  var arr = [23,34,54,0,4,7];
+  // var arr = [23,34,54,0,4,7];
 
 /*   function arrry(ar,maxi){
     var output = []
@@ -18,18 +18,22 @@ function App() {
   }
   console.log(arrry(arr,4)); */
 
-  function max(ar){
-    var max;
-    for(var i=0;i<=ar.length-1;i++){
-      max = ar[i];
-      if(ar[i]<max){
-        max=ar[i]
-      }
-    }
-    return max
-  }
-  console.log(max(arr));
- 
+  // function max(ar){
+  //   var max;
+  //   for(var i=0;i<=ar.length-1;i++){
+  //     max = ar[i];
+  //     if(ar[i]<max){
+  //       max=ar[i]
+  //     }
+  //   }
+  //   return max
+  // }
+  // console.log(max(arr));
+
+  var arr = [2,33,4,5,5,3,2,2,2,3,4,5,6,7,8,8,8]
+
+  var newArr= new Set([...arr])
+ console.log(newArr);
   return (
     <div className="App">
        HELLO
