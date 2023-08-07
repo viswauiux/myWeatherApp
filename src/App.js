3ffd671fdd0caa5118f7ab870403247606864a6a
viswakirankathi <viswauiux@gmail.com>
2023-08-07 21:28:00 +0530

    yes

--- src/App.js
+++ src/App.js
@@ -2,10 +2,8 @@ import './App.css';
 import { useEffect, useState } from 'react';
 
 function App() {
- for (let index = 0; index < 34; index++) {
-  
-  console.log("Viswa kiran kathi");
- }
+ 
+ 
   return (
     <div className="App">
       
