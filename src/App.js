6d36001479b5025e25faa87a1215ea29c00120f7
viswakirankathi <viswauiux@gmail.com>
2023-08-07 18:11:00 +0530

    yes

--- src/App.js
+++ src/App.js
@@ -4,19 +4,25 @@ import { useEffect, useState } from "react";
 import api from './Api/posts'
 
 function App() {
-  const [posts, setPosts] = useState();
-
+  const [data, setData] = useState();
+  const Api = "https://api.themoviedb.org/3/discover/movie"
+  const image = "https://images.tmdb.org/t/p/w500"
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const responce = await api.get("/quotes");
-        setPosts(responce.data);
-        console.log(responce.data.quotes.map(item=>console.log(item)));
+        const responce = await api.get(Api,{
+          params:{
+            api_key:'e5c140e5d7cddcc37ecdcf27a8535994'
+          }
+        });
+        console.log(responce.data.results)
+        setData(responce.data.results);
+        // console.log(responce.data.quotes.map(item=>console.log(item)));
       } catch (err) {
         if(err.responce){
          console.log(err.responce.status);
         }
-        console.log(err.message);
+        console.log(err);
       }
     };
     fetchData();
@@ -25,15 +31,9 @@ function App() {
     <div className="App " style={{display:"flex", flexDirection:"column", alignItems:"center"}}>
       
       {
-      posts && ( posts.quotes.map((item)=>{return(<div key={Math.random()} className="card border border-danger" style={{width:" 18rem"}}>
-        <div className="card-body">
-          <p className="card-text">
-           {item.quote}
-          </p>
-          <h5 className="card-title">Auther: {item.author}</h5>
-          <h3></h3>
-        </div>
-      </div>)}))
+        data && data.map(item=>{
+          return(<img key={item.id} src={image+item.backdrop_path} alt="" />)
+        })
       }
     </div>
   );
