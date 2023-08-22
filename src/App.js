b18b7b735ee2c6575786eb423176de3fe171a64d
viswakirankathi <viswauiux@gmail.com>
2023-08-22 07:57:41 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -3,7 +3,18 @@ import { useEffect, useState } from 'react';
 import Class from './Class';
 
 function App() {
-  
+
+    var ob1 = {name:"viswa"};
+
+    function abc(location){
+      console.log("Hello "+this.name+ " i am from "+location);
+    }
+    var bind2 = abc.bind(ob1)
+    bind2("London")
+    bind2("Hyderabad")
+
+ 
+ 
    
   return (
     <div className="App">
